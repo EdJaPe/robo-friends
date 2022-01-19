fc4be22b1cc7521887ad4fc16f137a3f9762f392
EdJaPe <ed.stu.ga.1214@gmail.com>
2022-01-19 12:13:58 -0800

    refactoring some code

--- src/containers/App.js
+++ src/containers/App.js
@@ -28,22 +28,20 @@ class App extends Component {
     const filteredRobots = robots.filter((robot) => {
       return robot.name.toLowerCase().includes(searchfield.toLowerCase());
     });
-    if (!robots.length) {
-      return <h1 className="tc f3 mt7">Loading...</h1>;
-    } else {
-      return (
-        <div className="tc">
-          <h1 className="f1">RoboFriends</h1>
-          <SearchBox
-            searchChange={this.onSearchChange}
-            searchField={searchfield}
-          />
-          <Scroll>
-            <CardList robots={filteredRobots} />
-          </Scroll>
-        </div>
-      );
-    }
+    return !robots.length ? (
+      <h1 className="tc f3 mt7">Loading...</h1>
+    ) : (
+      <div className="tc">
+        <h1 className="f1">RoboFriends</h1>
+        <SearchBox
+          searchChange={this.onSearchChange}
+          searchField={searchfield}
+        />
+        <Scroll>
+          <CardList robots={filteredRobots} />
+        </Scroll>
+      </div>
+    );
   }
 }
 export default App;
